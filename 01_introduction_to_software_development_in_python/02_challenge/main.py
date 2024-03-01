a67736c6c266de0334078f8c5e4ecbe02cce2684
yohei-ozao <yohei.ozao@woven-planet.global>
2024-03-01 15:26:04 +0900

    Submit challenge for grading

--- 01_introduction_to_software_development_in_python/02_challenge/main.py
+++ 01_introduction_to_software_development_in_python/02_challenge/main.py
@@ -3,71 +3,71 @@ import math
 
 # 2. 10と20の和を求めてください
 # ここにコードを書いてください
-sum_result = 
+sum_result = 10 + 20
 
 # 3. 3.5と2の積を求めてください
 # ここにコードを書いてください
-product_result = 
+product_result = 3.5 * 2
 
 # 4. 10を3で割り算した商を求めてください
 # ここにコードを書いてください
-division_result = 
+division_result = 10 / 3
 
 # 5. 10を3で割り算した余りを求めてください
 # ここにコードを書いてください
-remainder_result = 
+remainder_result = 10 % 3
 
 # 6. 2の3乗を求めてください
 # ここにコードを書いてください
-power_result = 
+power_result = 2 ** 3
 
 # 7. 25の平方根を求めてください
 # ヒント: math.sqrt() の使い方を調べることをおすすめします
 # ここにコードを書いてください
-sqrt_result = 
+sqrt_result = math.sqrt(25)
 
 # 8. -10の絶対値を求めてください
 # ヒント: abs() の使い方を調べることをおすすめします
 # ここにコードを書いてください
-absolute_value = 
+absolute_value = abs(-10)
 
 # 9. 10を3で整数除算 (切り捨て除算) した結果を求めてください
 # ここにコードを書いてください
-floor_division_result = 
+floor_division_result = 10 // 3
 
 # 10. 10、20、30の平均を求めてください
 # ここにコードを書いてください
-average_result = 
+average_result = (10 + 20 + 30) / 3
 
 # 11. 華氏98.6度を摂氏に変換してください
 fahrenheit = 98.6
 # ここにコードを書いてください
-celsius = 
+celsius = (fahrenheit - 32) * 5 / 9
 
 # 12. 縦5、横3の長方形の周囲の長さを求めてください
 length = 5
 width = 3
 # ここにコードを書いてください
-perimeter = 
+perimeter = (length + width) * 2
 
 # 13. 半径5の円の面積を求めてください
 # ヒント: math.piの使い方を調べることをおすすめします
 radius = 5
 # ここにコードを書いてください
-area_circle = 
+area_circle = radius ** 2 * math.pi
 
 # 14. 時速100kmは秒速何メートルですか
 km_per_hour = 100
 # ここにコードを書いてください
-meters_per_second = 
+meters_per_second = km_per_hour * (1000 / (60 ** 2))
 
 # 15. 直角三角形があり、2つの辺の長さはそれぞれ3と4です。この三角形の斜辺の長さを求めてください
 side1 = 3
 side2 = 4
 # ここにコードを書いてください
-hypotenuse = 
+hypotenuse = math.sqrt(side1 ** 2 + side2 ** 2)
 
 # 16. 半径5の球の体積を求めてください
 radius_sphere = 5
 # ここにコードを書いてください
-volume_sphere = 
+volume_sphere = radius_sphere ** 3 * math.pi * 4 / 3
