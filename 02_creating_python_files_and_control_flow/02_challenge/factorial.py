61195687e3bd9bfdcd821e2c583e8845f01cda1e
yohei-ozao <yohei.ozao@woven-planet.global>
2024-03-08 14:39:13 +0900

    Submit challenge for grading

--- 02_creating_python_files_and_control_flow/02_challenge/factorial.py
+++ 02_creating_python_files_and_control_flow/02_challenge/factorial.py
@@ -1,12 +1,19 @@
 def compute_factorial():
     # ここにコードを書いてください
     # number変数を編集し、ユーザー入力として正の整数を受け取ります。整数に変換することを忘れないでください
-    number = 0
+    number = int(input()) # "5"とかは
+    if number == 0:
+        return 1
 
+    # loop
+    count = number
+    while (count > 1):
+        number = number * (count - 1)
+        count = count - 1
+    
     # result変数を編集し、最終的な計算結果を保存します
-    result = 0
+    result = number
 
     return result
-
 
 compute_factorial()
