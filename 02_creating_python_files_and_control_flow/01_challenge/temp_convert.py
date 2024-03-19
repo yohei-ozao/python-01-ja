d419db14b95aacf52b67c1196ab7a402d718f852
yohei-ozao <yohei.ozao@woven-planet.global>
2024-03-19 14:58:45 +0900

    Submit challenge for grading

--- 02_creating_python_files_and_control_flow/01_challenge/temp_convert.py
+++ 02_creating_python_files_and_control_flow/01_challenge/temp_convert.py
@@ -1,7 +1,20 @@
 def convert():
     # ここにコードを書いてください
     # temp変数を編集し、ユーザー入力として温度を受け取ります。整数に変換することを忘れないでください
-    temp = 0
+    temp = float(input("Enter the temprature:"))
+    unit = str(input("Enter the unit:"))
+    if (unit == "f"):
+        unitc = str("c")
+        tempc = (temp - 32) * 5 / 9
+        result = str(tempc) + unitc
+        print(result)
+        temp = tempc
+    elif (unit == "c"):
+        unitf = str("f")
+        tempf = (temp * 9 / 5) + 32
+        result = str(tempf) + unitf
+        print(result)
+        temp = tempf
 
     return temp
 
--- 02_creating_python_files_and_control_flow/02_challenge/factorial.py
+++ 02_creating_python_files_and_control_flow/02_challenge/factorial.py
@@ -1,16 +1,22 @@
 def compute_factorial():
     # ここにコードを書いてください
     # number変数を編集し、ユーザー入力として正の整数を受け取ります。整数に変換することを忘れないでください
-    number = int(input()) # "5"とかは
+    number = int(input()) 
     if number == 0:
         return 1
 
-    # loop
+    # while loop
     count = number
     while (count > 1):
         number = number * (count - 1)
         count = count - 1
-    
+
+    '''
+    # for loop
+    for oneNumber in range(1, number):
+        number = number * oneNumber
+    '''
+        
     # result変数を編集し、最終的な計算結果を保存します
     result = number
 
