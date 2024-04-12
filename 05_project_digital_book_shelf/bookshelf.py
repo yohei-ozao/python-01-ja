ebb8dde70ccca32d5250e49cc18538e5542c8956
yohei-ozao <yohei.ozao@woven-planet.global>
2024-04-12 13:33:11 +0900

    Submit challenge for grading

--- 05_project_digital_book_shelf/bookshelf.py
+++ 05_project_digital_book_shelf/bookshelf.py
@@ -1,18 +1,18 @@
-import numpy
-import random
+from tabulate import tabulate
+import sys
 import math
 
+#表示するメッセージの定義
 welcometitle = "Welcome to your personal books digital library!"
 flag = "=" * (len(welcometitle) + 1)
 confirmselection = "You selected: "
 addtitlemessage = "Please input the book title: "
 addunreadmessage = "Please input read or unread: "
+edittitlemessage = "Please input the book title you want to edit: "
 
 
-# Define dict and status
+# 辞書とリストの初期化
 bookshelf = []
-bookdata = {}
-read_status_data = {}
 id = int()
 title = str()
 read_status = bool(True)
@@ -22,8 +22,9 @@ print(flag)
 print(welcometitle)
 print(flag)
 
-# Define funciton
-def usrops():
+# 関数を定義
+#最初の選択画面の表示とユーザー入力を促す
+def usrops(): 
     while True:
         # User input
         selecitonlist = ("", "1: Add a Book", "2: Edit a Book", "3: Search for Books", "4: Delte a Book", "5: Videw Library Stats", "6: Exit app", )
@@ -47,48 +48,114 @@ def usrops():
         if (usrinput.isdecimal()) and (int(usrinput) >= 1 and int(usrinput) <= 7):
             usrinput = int(usrinput)
             print(confirmselection + selecitonlist[usrinput])
-            return usrinput
             break
-        print(selectcomment)
+            print(selectcomment)
+    return usrinput
 
-def addbook():
+#本を追加する関数
+def addbook(): 
+    while True:
+        #本を追加するメッセージ
+        print(addtitlemessage)
+        title = str(input())
+        #既読・未読の正しい入力を判断
+        while True:
+            print(addunreadmessage)
+            read_status_str = str(input())
+            if read_status_str != "read" and read_status_str != "unread":
+                continue
+            else:
+                if read_status_str == "read":
+                    read_status = True
+                    break
+                elif read_status_str == "unread":
+                    read_status = False
+                    break
+
+        idxlen = len(bookshelf) + 1
+
+        #bookshelfが空の場合に入力をインデックス0に配置
+        if not bookshelf:
+            bookdata = {"id":0, "title":title, "status":read_status}
+            bookshelf.append(bookdata)
+            for row in bookshelf:
+                idlist = [row["id"]]
+                titlelist =[row["title"]]
+        #bookshelfがからでない場合、空いている一番小さいインデックス番号にデータを配置
+        elif len(bookshelf) != 0:
+            idlist = [i.get("id") for i in bookshelf]
+            titlelist = [i.get("title") for i in bookshelf]
+            #bookshelf内に同じタイトルがあった場合は再入力を促す
+            for id in range(0, idxlen):
+                if (title in titlelist):
+                    print("There is a book that is the same title as you entered")
+                    break
+                #bookshelf内に同じタイトルがなかったらデータを配置
+                elif (title not in titlelist and id not in idlist):
+                    bookdata = {"id":id, "title":title, "status":read_status}
+                    bookshelf.insert(id, bookdata)
+                    #elif (id in idlist):
+                    #        id = id + 1
+        print(tabulate(bookshelf))
+        #print(bookshelf) #チェック用
+        #print("id is: " + str(id)) #チェック用
+        #print("idxlen is: " + str(idxlen)) #チェック用
+        return bookshelf
+
+#本を編集する関数
+def editbook():
+    bookdata = {}
+    print(edittitlemessage)
+    title = str(input())
+    list_search = list(filter(lambda item : item["title"] == title, bookshelf))
+    bookdata = list_search[0]
+    id = bookdata["id"]
+    bookshelf.pop(id)
+    #本を追加するメッセージ
     print(addtitlemessage)
     title = str(input())
-    while (True):
+    #既読・未読の正しい入力を判断
+    while True:
         print(addunreadmessage)
-        read_status = str(input())
-        if (read_status == "read" or read_status == "unread"):
-            read_status = read_status
-            break
-    id = int(0)
-    #print(bookdata)
-    idxlen = len(bookshelf) + 1
-    print("id is: " + str(id))
-    print("idxlen is: " + str(idxlen))
-    for id in range (id, idxlen):
-        if (id not in bookdata):
-            bookdata[id] = {title:read_status}
-            bookshelf.insert(id, bookdata)
-            print(bookshelf)
-        break
-        
+        read_status_str = str(input())
+        if read_status_str != "read" and read_status_str != "unread":
+            continue
+        else:
+            if read_status_str == "read":
+                read_status = True
+                break
+            elif read_status_str == "unread":
+                read_status = False
+                break
+    bookdata = {"id":id, "title":title, "status":read_status}
+    bookshelf.insert(id,bookdata)
+    print(tabulate(bookshelf))
+    #print(bookdata["id"]) #チェック用
     return
 
-def editbook():
+#本を検索する関数
+def searchbook(): 
     return
 
-def searchbook():
+#本を消去する関数
+def deletebook(): 
     return
 
-def deletebook():
+#統計データを見る関数
+def libstats(): 
     return
 
-def libstats():
+#bookshelf.pyを修了する関数
+def exit(): 
+    sys.exit()
     return
 
-usrinput = usrops()
+#ユーザー入力の初期化
+usrinput = int()
 
+#操作選択画面を継続的に表示
 while (usrinput != 6):
+    usrinput = usrops()
     if usrinput == 1:
         addbook()
     elif usrinput == 2:
@@ -101,4 +168,4 @@ while (usrinput != 6):
         libstats()
     elif usrinput == 6:
         exit()
-    usrinput = usrops()
+    
