e3559e660ccc84e4e79ec7fd291c585bd0309611
yohei-ozao <yohei.ozao@woven-planet.global>
2024-04-18 21:08:19 +0900

    Submit for grading

--- 03_basic_data_structures_and_file_handling/01_challenge/counter.py
+++ 03_basic_data_structures_and_file_handling/01_challenge/counter.py
@@ -14,7 +14,16 @@ def counter():
     ]
 
     # ここにコードを書いてください
-
+    c_banana = fruits.count("banana")
+    c_apple = fruits.count("apple")
+    c_orange = fruits.count("orange")
+    c_grape = fruits.count("grape")
+    c_melon = fruits.count("melon")
+    c_kiwi = fruits.count("kiwi")
+    c_strawberry = fruits.count("strawberry")
+    occurrences = {"banana":str(c_banana), "apple":str(c_apple), "orange":str(c_orange), "grape":str(c_grape), "melon":str(c_melon), "kiwi":str(c_kiwi), "strawberry":str(c_strawberry)}
+    for key,value in occurrences.items():
+        print(str(key) + ": " + str(value))
     return occurrences
 
 print(counter())
