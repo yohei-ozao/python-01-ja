d0c355aa6cbd13e2f4dcbb51e2c8d4a5f578e755
yohei-ozao <yohei.ozao@woven-planet.global>
2024-04-19 00:58:11 +0900

    Submit for grading

--- 04_basic_functions/01_challenge/calculator.py
+++ 04_basic_functions/01_challenge/calculator.py
@@ -1,14 +1,38 @@
 def add(a, b):
-    return 0
+    if type(a) is int and type(b) is int:
+        result = int(a) + int(b)
+        return result
+    else:
+        return "bad input"
 
 
 def subtract(a, b):
-    return 0
+    if type(a) is int and type(b) is int:
+        result = int(a) - int(b)
+        return result
+    else:
+        return "bad input"
 
 
 def multiply(a, b):
-    return 0
+    if type(a) is int and type(b) is int:
+        result = int(a) * int(b)
+        return result
+    else:
+        return "bad input"
 
 
 def divide(a, b):
-    return 0
+    import numpy as np
+    if type(a) is int and type(b) is int:
+        try: 
+            return int(a) / int(b) 
+        except ZeroDivisionError:
+            return "Cannot divide by zero"
+    else:
+        return "bad input"
+
+# a = input("a: ")
+# b = input("b: ")
+# print(add(a,b))
+#print(add("5", 5))
