c2a0b4c9d97f393462a712810d9b01ab655ab1a5
yohei-ozao <yohei.ozao@woven-planet.global>
2024-04-18 22:50:06 +0900

    Submit for grading

--- 03_basic_data_structures_and_file_handling/03_challenge/ls_command.py
+++ 03_basic_data_structures_and_file_handling/03_challenge/ls_command.py
@@ -6,7 +6,34 @@ def ls_command(directory):
     # 無効なディレクトリパスについて考える必要はありません
 
     # 「pass」を削除して、ここにコードを書いてください
-    pass
+    import collections
+    ext = []
+    directory_path = directory
+    files = os.listdir(directory_path)
+    files = sorted(files)
+    # ファイル名から拡張子を取り出し、それをカウントする辞書
+    extension_count = {}
+
+    for file in files:
+        # '.'で分割し、最後の要素を拡張子とする
+        extension = "." + file.split('.')[-1]
+
+        # 辞書に拡張子がまだ存在しない場合は、新たにキーを作成し、カウントを1にする
+        if extension not in extension_count:
+            extension_count[extension] = 1
+        # 辞書に拡張子がすでに存在する場合は、カウントを1増やす
+        else:
+            extension_count[extension] += 1
+    #print(extension_count)
+    #print(files) 
+    print("Summary in alphabetical order:")
+    for key,value in extension_count.items():
+        if int(value) == 1:
+            print(str(key) + ":" + str(value) + " file")
+        elif int(value) >= 2:
+            print(str(key) + ":" + str(value) + " files")
+    
+    return
 
 
 if __name__ == "__main__":
